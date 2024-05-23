e9364fc4a26706c268e9d45ad85ef029bf9130cd
rs-ventana <ruac.rs@outlook.com>
2024-05-23 10:50:00 -0600

    Previo a la presentación

--- pagComunidad/nuevoJS.js
+++ pagComunidad/nuevoJS.js
@@ -56,7 +56,7 @@ const tarjetas = [
         travelAdvice: "En Brasil, la ciudad de Río de Janeiro es una visita obligada. Conocida por su icónica estatua del Cristo Redentor y las impresionantes playas de Copacabana e Ipanema, Río ofrece una mezcla de belleza natural y cultura vibrante. No olvides tomar un teleférico hasta el Pan de Azúcar para disfrutar de vistas impresionantes de la ciudad.",
         travelPhotos: ["./avion.jpg"],
         location: "Río de Janeiro, Brasil",
-        reco: "reseña",
+        reco: "Reseña",
         tags: ["Extranjero","Río de Janeiro", "Cristo Redentor", "Copacabana", "Pan de Azúcar"]
         
       },
@@ -68,7 +68,7 @@ const tarjetas = [
         travelAdvice: "En México, asegúrate de explorar la rica historia de la Ciudad de México. Visita el Zócalo, la plaza principal de la ciudad, donde puedes ver la Catedral Metropolitana y el Palacio Nacional. Las cercanas ruinas de Teotihuacán también son una visita obligada, con la impresionante Pirámide del Sol. No olvides explorar los vibrantes barrios de Coyoacán y Roma.",
         travelPhotos: ["mar.jpg"],
         location: "CDMX, México",
-        reco: "reseña",
+        reco: "Reseña",
         tags: ["Local", "Ciudad de México", "Zócalo", "Teotihuacán", "Coyoacán"]
         
       },
@@ -80,7 +80,7 @@ const tarjetas = [
         travelAdvice: "Visita el Desierto de Atacama en Chile para una experiencia realmente única. El Valle de la Luna es un paisaje impresionante que se asemeja a la superficie de la luna. También puedes visitar los géiseres de El Tatio, el tercer campo de géiseres más grande del mundo, y los impresionantes salares de Salar de Atacama.",
         travelPhotos: ["mar.jpg"],
         location: "Desierto de Atacama, Chile",
-        reco: "reseña",
+        reco: "Reseña",
         tags: ["Extranjero", "Desierto de Atacama", "Valle de la Luna", "El Tatio", "Salar de Atacama"]
   
       },
@@ -92,7 +92,7 @@ const tarjetas = [
         travelAdvice: "Costa Rica es un paraíso para los amantes de la naturaleza. Visita el Parque Nacional Manuel Antonio por sus hermosas playas y abundante vida silvestre. El Volcán Arenal es otro lugar imprescindible, ofreciendo aguas termales y oportunidades para hacer senderismo. No te pierdas el Bosque Nuboso de Monteverde por su ecosistema único y emocionantes tours de canopy.",
         travelPhotos: ["volcan.jpg"],
         location: "Costa Rica",
-        reco: "reseña",
+        reco: "Reseña",
         tags: ["Extranjero", "Manuel Antonio", "Volcán Arenal", "Monteverde"]
    
       },
@@ -104,7 +104,7 @@ const tarjetas = [
         travelAdvice: "En Ecuador, un viaje a las Islas Galápagos es una experiencia única en la vida. La fauna única y los impresionantes paisajes no tienen comparación. Explora las islas en barco, haz snorkel con lobos marinos y visita la Estación de Investigación Charles Darwin. Las tierras altas de la Isla Santa Cruz ofrecen hermosas oportunidades para hacer senderismo.",
         travelPhotos: ["avion.jpg"],
         location: "Islas Galápagos, Ecuador",
-        reco: "reseña",
+        reco: "Reseña",
         tags: ["Local", "Islas Galápagos", "Fauna", "Isla Santa Cruz"]
    
       },
@@ -116,7 +116,7 @@ const tarjetas = [
         travelAdvice: "En Bolivia, el Salar de Uyuni es un destino imperdible. El salar más grande del mundo ofrece un paisaje surrealista, especialmente durante la temporada de lluvias cuando se convierte en un gigantesco espejo. También puedes visitar el cercano Parque Nacional Eduardo Avaroa para ver flamencos y otras especies en impresionantes entornos naturales.",
         travelPhotos: ["mar.jpg"],
         location: "Salar de Uyuni, Bolivia",
-        reco: "reseña",
+        reco: "Reseña",
         tags: ["Extranjero", "Salar de Uyuni", "Parque Nacional Eduardo Avaroa", "Flamencos"]
        
       },
@@ -128,9 +128,8 @@ const tarjetas = [
         travelAdvice: "La Ciudad de Panamá es un destino vibrante y cosmopolita. El histórico distrito de Casco Viejo está lleno de hitos de la era colonial y cafés de moda. No te pierdas una visita al Canal de Panamá, una de las mayores hazañas de ingeniería del siglo XX. El cercano Parque Nacional Soberanía ofrece excelentes oportunidades para la observación de aves y el senderismo.",
         travelPhotos: ["playa.jpg"],
         location: "Ciudad de Panamá, Panamá",
-        reco: "recomendación",
+        reco: "Recomendación",
         tags: ["Local", "Casco Viejo", "Canal de Panamá", "Parque Nacional Soberanía"]
-    
       },
 
 ];
@@ -168,9 +167,20 @@ function crearTarjeta(tarjeta) {
   cardLocation.innerHTML = `<small class="text-muted">${tarjeta.location}</small>`;
 
   const cardReco = document.createElement('span'); 
-  cardReco.className = 'tag badge badge-info';
   cardReco.textContent = tarjeta.reco;
-
+  
+  // Agregar clases según la palabra clave 'reco'
+  if (tarjeta.reco === 'Recomendación') {
+      cardReco.classList.add('badge', 'badge-recomendacion');
+  } else if (tarjeta.reco === 'Advertencia') {
+      cardReco.classList.add('badge', 'badge-advertencia');
+  } else if (tarjeta.reco === 'Reseña') {
+      cardReco.classList.add('badge', 'badge-resena');
+  }
+  
+  cardLocation.appendChild(document.createElement('br'));
+  cardLocation.appendChild(cardReco);
+  
   const tags = document.createElement('div');
   tags.className = 'tags';
   tarjeta.tags.forEach(tag => {
@@ -180,15 +190,51 @@ function crearTarjeta(tarjeta) {
       tags.appendChild(tagItem);
   });
 
+  // Crear los botones adicionales
+  const additionalButtons1 = document.createElement("button");
+  additionalButtons1.type = "button";
+  additionalButtons1.className = "btn btn-primary btn-sm";
+  additionalButtons1.classList.add("bn-sl");
+  additionalButtons1.textContent = "Seguir leyendo";
+
+  const additionalButtons2 = document.createElement("button");
+  additionalButtons2.type = "button";
+  additionalButtons2.className = "btn btn-primary btn-sm";
+  additionalButtons2.classList.add("bn-mg");
+  additionalButtons2.textContent = "Me gusta";
+
+  // Crear el dropdown
+  const dropdown = document.createElement("div");
+  dropdown.classList.add("dropdown");
+  dropdown.innerHTML = `
+    <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <i class="bi bi-three-dots-vertical"></i>
+    </button>
+    <ul class="dropdown-menu dropdown-menu-dark">
+      <li><a class="dropdown-item active" href="#"><i class="bi bi-bookmark-plus"></i> Guardar publicación</a></li>
+      <li><a class="dropdown-item" href="#"><i class="bi bi-trash3"></i> Eliminar publicación</a></li>
+      <li><a class="dropdown-item" href="#"><i class="bi bi-exclamation-circle"></i> Reportar publicación</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><a class="dropdown-item" href="#"><i class="bi bi-share"></i> Compartir</a></li>
+    </ul>
+  `;
+
+   // Crear un contenedor flex para los botones y el dropdown
+   const buttonsContainer = document.createElement("div");
+   buttonsContainer.className = "d-flex align-items-center mb-3";
+   // Añadir los botones y el dropdown al contenedor
+   buttonsContainer.appendChild(additionalButtons1);
+   buttonsContainer.appendChild(additionalButtons2);
+   buttonsContainer.appendChild(dropdown);
+
   cardBody.appendChild(cardTitle);
   cardBody.appendChild(profilePhoto);
   cardBody.appendChild(cardUsername);
- 
   cardBody.appendChild(cardAdvice);
   cardBody.appendChild(cardLocation);
   cardBody.appendChild(cardReco);
   cardBody.appendChild(tags);
-
+  cardBody.appendChild(buttonsContainer);
   card.appendChild(travelPhoto);
   card.appendChild(cardBody);
 
