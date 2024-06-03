f7d638b6febd87e73b89026d7326e51408a85c82
rs-ventana <ruac.rs@outlook.com>
2024-06-02 18:31:04 -0600

    De nuevo, version final y depuración de insumos

--- js/Comunidad-Publicaciones.js
+++ js/Comunidad-Publicaciones.js
@@ -190,7 +190,7 @@ card.innerHTML = `
 <img src="${post.image}" class="card-img-top" alt="Imagen adjunta" style="display: ${post.image ? 'block' : 'none'};">
 <div class="card-body">
     <div class="user-profile d-flex align-items-center mb-3">
-        <img src="../public/insumos/mujer.jpg" alt="Perfil" class="rounded-circle me-3" width="50">
+        <img src="../public/insumos/mujer2.jpg" alt="Perfil" class="rounded-circle me-3" width="50">
         <div>
             <p class="mb-0">Laura Vargas</p>
             <p class="mb-0">@traveler11</p>
