44424b976249b8530e71d88930cffb7a8fbf68bc
DianaHang <dianavegat99@gmail.com>
2024-06-04 00:34:30 -0600

    update modal, carrousel

--- js/Home-Carrousel.js
+++ js/Home-Carrousel.js
@@ -4,7 +4,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   let carouselElement = document.getElementById('carouselHome');
   let carousel = new bootstrap.Carousel(carouselElement, {
-    interval: 4000,
+    interval: 20000,
     ride: 'carousel'
   });
 
@@ -19,7 +19,21 @@ document.addEventListener('DOMContentLoaded', function() {
   buttonNext.addEventListener('click', function() {
     carousel.next();
   });
-}); 
+
+  // Reiniciar videos al cambiar de slide
+  carouselElement.addEventListener('slid.bs.carousel', function(event) {
+    let videos = carouselElement.querySelectorAll('video');
+    videos.forEach(video => {
+      video.pause();
+      video.currentTime = 0;
+    });
+
+    let activeVideo = event.relatedTarget.querySelector('video');
+    if (activeVideo) {
+      activeVideo.play();
+    }
+  });
+});
 
 
 //Función para validar datos del modal de inicio de sesión
@@ -27,11 +41,12 @@ function acceso() {
   const email=document.getElementById("email").value;
   const password=document.getElementById("password").value;
   
-  if(email=="algo@otracosa.com" && password==="probandoando")
-      {
-          window.location="Comunidad.html"
+  if(email=="algo@otracosa.com" && password==="probandoando") { 
+    //Guardar el estado de inicio de sesión en localStorage
+      localStorage.setItem('isLoggedIn', 'true');
+          window.location.href="Comunidad.html";
       } else {
           alert("Datos incorrectos")
       }
-  
   }
+
