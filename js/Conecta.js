4190f23a3dccdc62c4dc2908035e0a07f10c2920
Cecilia-Riverol <sandra.ruverol@gmail.com>
2024-06-05 01:34:03 -0600

    Pagina conecta

---
+++ js/Conecta.js
@@ -0,0 +1,210 @@
+const paisesLatinoAmerica = [
+  {
+    nombre: "Argentina",
+    capital: "Buenos Aires",
+    idiomasOficiales: "Español",
+    moneda: "Peso Argentino",
+    clima: "Variado (Subtropical en el Norte, Subpolar en el Sur)",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo C/I",
+    atraccionesTuristicas: "Cataratas del Iguazú, Glaciar Perito Moreno, Buenos Aires, Bariloche"
+  },
+  {
+    nombre: "Bolivia",
+    capital: "Sucre (constitucional), La Paz (administrativa)",
+    idiomasOficiales: "Español, Quechua, Aymara, Guaraní, y 33 otros idiomas indígenas",
+    moneda: "Boliviano",
+    clima: "Variado (Tropical en el Este, Frío en el Oeste)",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/C",
+    atraccionesTuristicas: "Salar de Uyuni, Lago Titicaca, La Paz, Parque Nacional Madidi"
+  },
+  {
+    nombre: "Brasil",
+    capital: "Brasilia",
+    idiomasOficiales: "Portugués",
+    moneda: "Real Brasileño",
+    clima: "Variado (Tropical en el Norte, Templado en el Sur)",
+    nivelSeguridadPublica: "Medio a Alto",
+    tipoEnchufe: "Tipo N",
+    atraccionesTuristicas: "Cristo Redentor, Cataratas del Iguazú, Selva Amazónica, Río de Janeiro"
+  },
+  {
+    nombre: "Chile",
+    capital: "Santiago",
+    idiomasOficiales: "Español",
+    moneda: "Peso Chileno",
+    clima: "Variado (Desierto en el Norte, Mediterráneo en el Centro, Subpolar en el Sur)",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo C/L",
+    atraccionesTuristicas: "Desierto de Atacama, Torres del Paine, Isla de Pascua, Valparaíso"
+  },
+  {
+    nombre: "Colombia",
+    capital: "Bogotá",
+    idiomasOficiales: "Español",
+    moneda: "Peso Colombiano",
+    clima: "Tropical y Variado según la altitud",
+    nivelSeguridadPublica: "Medio a Bajo",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Cartagena, Medellín, Parque Nacional Natural Tayrona, Bogotá"
+  },
+  {
+    nombre: "Costa Rica",
+    capital: "San José",
+    idiomasOficiales: "Español",
+    moneda: "Colón Costarricense",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Parque Nacional Manuel Antonio, Monteverde, Arenal, San José"
+  },
+  {
+    nombre: "Cuba",
+    capital: "La Habana",
+    idiomasOficiales: "Español",
+    moneda: "Peso Cubano",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/B/C",
+    atraccionesTuristicas: "La Habana Vieja, Varadero, Trinidad, Viñales"
+  },
+  {
+    nombre: "Ecuador",
+    capital: "Quito",
+    idiomasOficiales: "Español, Quichua",
+    moneda: "Dólar Estadounidense",
+    clima: "Variado (Tropical en la costa y Amazonía, Templado en la Sierra)",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Islas Galápagos, Quito, Cuenca, Baños"
+  },
+  {
+    nombre: "El Salvador",
+    capital: "San Salvador",
+    idiomasOficiales: "Español",
+    moneda: "Dólar Estadounidense",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Medio a Bajo",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Ruta de las Flores, San Salvador, Suchitoto, El Boquerón"
+  },
+  {
+    nombre: "Guatemala",
+    capital: "Ciudad de Guatemala",
+    idiomasOficiales: "Español",
+    moneda: "Quetzal",
+    clima: "Tropical y Templado según la altitud",
+    nivelSeguridadPublica: "Medio a Bajo",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Antigua, Tikal, Lago de Atitlán, Semuc Champey"
+  },
+  {
+    nombre: "Honduras",
+    capital: "Tegucigalpa",
+    idiomasOficiales: "Español",
+    moneda: "Lempira Hondureño",
+    clima: "Tropical en la costa, Templado en las tierras altas",
+    nivelSeguridadPublica: "Bajo",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Copán, Islas de la Bahía, La Ceiba, Tegucigalpa"
+  },
+  {
+    nombre: "México",
+    capital: "Ciudad de México",
+    idiomasOficiales: "Español",
+    moneda: "Peso Mexicano",
+    clima: "Variado (Desierto en el Norte, Tropical en el Sur)",
+    nivelSeguridadPublica: "Medio a Bajo",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Ciudad de México, Cancún, Chichén Itzá, Tulum"
+  },
+  {
+    nombre: "Nicaragua",
+    capital: "Managua",
+    idiomasOficiales: "Español",
+    moneda: "Córdoba Nicaragüense",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Granada, Isla de Ometepe, León, San Juan del Sur"
+  },
+  {
+    nombre: "Panamá",
+    capital: "Ciudad de Panamá",
+    idiomasOficiales: "Español",
+    moneda: "Balboa, Dólar Estadounidense",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Canal de Panamá, Casco Viejo, Bocas del Toro, San Blas"
+  },
+  {
+    nombre: "Paraguay",
+    capital: "Asunción",
+    idiomasOficiales: "Español, Guaraní",
+    moneda: "Guaraní Paraguayo",
+    clima: "Subtropical a templado",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo C",
+    atraccionesTuristicas: "Asunción, Encarnación, Itaipú, Parque Nacional Ybycuí"
+  },
+  {
+    nombre: "Peru",
+    capital: "Lima",
+    idiomasOficiales: "Español, Quechua, Aymara",
+    moneda: "Sol",
+    clima: "Variado (Desértico en la costa, Tropical en la selva)",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/C",
+    atraccionesTuristicas: "Machu Picchu, Lima, Cusco, Lago Titicaca"
+  },
+  {
+    nombre: "República Dominicana",
+    capital: "Santo Domingo",
+    idiomasOficiales: "Español",
+    moneda: "Peso Dominicano",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo A/B",
+    atraccionesTuristicas: "Punta Cana, Santo Domingo, Puerto Plata, Bayahibe"
+  },
+  {
+    nombre: "Uruguay",
+    capital: "Montevideo",
+    idiomasOficiales: "Español",
+    moneda: "Peso Uruguayo",
+    clima: "Templado",
+    nivelSeguridadPublica: "Medio",
+    tipoEnchufe: "Tipo C/F",
+    atraccionesTuristicas: "Punta del Este, Montevideo, Colonia del Sacramento, Piriápolis"
+  },
+  {
+    nombre: "Venezuela",
+    capital: "Caracas",
+    idiomasOficiales: "Español",
+    moneda: "Bolívar Soberano",
+    clima: "Tropical",
+    nivelSeguridadPublica: "Bajo",
+    tipoEnchufe: "Tipo A/B/C",
+    atraccionesTuristicas: "Salto Ángel, Los Roques, Mérida, Caracas"
+  }
+];
+
+
+function showCountryDetails(countryName) {
+  const country = paisesLatinoAmerica.find(pais => pais.nombre === countryName);
+
+  if (country) {
+      document.getElementById('country-name').textContent = country.nombre;
+      document.getElementById('country-capital').textContent = country.capital;
+      document.getElementById('country-languages').textContent = country.idiomasOficiales;
+      document.getElementById('country-currency').textContent = country.moneda;
+      document.getElementById('country-climate').textContent = country.clima;
+      document.getElementById('country-security').textContent = country.nivelSeguridadPublica;
+      document.getElementById('country-plug').textContent = country.tipoEnchufe;
+      document.getElementById('country-attractions').textContent = country.atraccionesTuristicas;
+      document.getElementById('country-details').style.display = 'block';
+  }
+
+}
