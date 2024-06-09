1c803118debd3919c034627e26d779240a3d8335
Blangis <blancag.licona@gmail.com>
2024-06-08 19:56:52 -0600

    diseño en pag de registro y ajuste de funcionalidades

--- js/Registro.js
+++ js/Registro.js
@@ -14,18 +14,19 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const inputPassword = document.getElementById("passwords");
     const inputPasswordcopy = document.getElementById("passwordClone");
     
-    
     const alertaError= document.querySelector(".alert-danger");
     const alertaExito = document.querySelector(".alert-success");
+    const alertaCorreo = document.querySelector(".alert-warning");
+    const alertaUser = document.querySelector(".alert-info");
     
     formRegister.addEventListener("submit", (event) => {
         event.preventDefault(); // Evitar el envío del formulario
-
+        
         const formData = {
             name: inputUser.value,
             lastname: inputLastNames.value,
-            nickname: inputNickname.value,
             email: inputEmail.value,
+            nickname: inputNickname.value,
             phone: inputPhone.value,
             age: inputAge.value,
             country: selectResidence.value,
@@ -35,6 +36,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
         };
 
         console.log(formData); // Imprimir los datos en la consola
+
+        
     
         // Cargar los datos del almacenamiento local al cargar la página
         let storedFormData = JSON.parse(localStorage.getItem('formData')) || [];
@@ -45,37 +48,51 @@ document.addEventListener("DOMContentLoaded", ()=> {
             storedFormData = [];
         }
 
-        // Verificar si el correo de usuario ya está registrado
+        // Verificar si el corre de usuario ya está registrado
         const usuarioRegistrado = storedFormData.find(user => user.email === formData.email);
-        if (usuarioRegistrado) {
-            alert("Ya hay una cuenta registrada con ese correo o hay errores en el formulario");
-            return;
-              
+        //Verificar si el nickname de usuaria ya está registrado
+        const nicknameOcupado = storedFormData.find(user => user.nickname === formData.nickname);
+
+        if (usuarioRegistrado !== undefined && usuarioRegistrado !== null && usuarioRegistrado !== "") {
+            alertaError.classList.remove("alertaError");
+            alertaUser.classList.remove("alertaUser");
+            alertaCorreo.classList.add("alertaCorreo");
+            setTimeout(() =>{
+                alertaCorreo.classList.remove("alertaCorreo");
+            }, 3000);  
+            
+        } else if (nicknameOcupado !== undefined && nicknameOcupado !== null && nicknameOcupado !== "") {
+            alertaError.classList.remove("alertaError");
+            alertaCorreo.classList.remove("alertaCorreo");
+            alertaUser.classList.add("alertaUser");
+            setTimeout(() =>{
+                alertaUser.classList.remove("alertaUser");
+            }, 3000);  
+            
+        } else  {
+            alertaError.classList.add("alertaError");
+            enviarFormulario(formData, storedFormData);
+            /* Agregar los nuevos datos al array y guardarlos en localStorage
+            storedFormData.push(formData);
+            localStorage.setItem('formData', JSON.stringify(storedFormData));*/
+           
+            //alertaExito.classList.add("alertaExito");
+            
         } 
 
-        //Verificar que el @usuario no esté repetido
-        const usuarioNickname = storedFormData.find(user => user.nickname === formData.nickname);
-        if (usuarioNickname) {
-            alert ("Ya está ocupado ese nombre de usuaria, por favor elige otro");
-            return;
-        }
+        //enviarFormulario();
         
-            // Agregar los nuevos datos al array y guardarlos en localStorage
-            storedFormData.push(formData);
-            localStorage.setItem('formData', JSON.stringify(storedFormData));
-           
-            alert("Registro exitoso");
-            window.location.href = "Iniciar-Sesion.html";
-        
-    });
+     }); 
         
     /*Expresiones regulares. Son reglas de validación. La primera permite uno o dos nombres, de 2 a 12 caracteres cada uno, cualquier letra en minúscula o mayúscula*/
-    const nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚüÜ]{3,10}(?:\s[a-zA-ZáéíóúÁÉÍÓÚüÜ]{0,10})?$/;
+    const nameRegex = /^(?=.{3,30}$)([A-Za-zÁÉÍÓÚáéíóúñÑ]{1,10}\s?){1,3}$/
+    const lastnameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚüÜ]{3,10}(?:\s[a-zA-ZáéíóúÁÉÍÓÚüÜ]{0,20}\s?)?$/;
     const nickRegex = /^@[A-Za-z0-9._/-]{5,}$/;
     const emailRegex = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const passwordRegex = /^(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*\d)[a-zA-Z\dñÑ!@#$%^&*(),.?":{}|<>]{8,}$/;
-    const cityRegex= /^[a-zA-ZáéíóúÁÉÍÓÚüÜ\s]{5,}$/;
+    const cityRegex= /^[a-zA-ZáéíóúÁÉÍÓÚüÜ\s]{4,}$/;
     const phoneRegex= /^\+?(\d{1,4})?[\s-]?(\(?\d{2,3}\)?)?[\s-]?\d{4,5}[\s-]?\d{4}$/;
+    
     
     
     
@@ -85,7 +102,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
         });
     
         inputLastNames.addEventListener ("input", () =>{
-            validarCampo(nameRegex, inputLastNames, "El apellido debe tener mínimo 3 caracteres, sin caracteres especiales");
+            validarCampo(lastnameRegex, inputLastNames, "El apellido debe tener mínimo 3 caracteres, sin caracteres especiales");
         });
 
         inputNickname.addEventListener ( "input", () => {
@@ -116,12 +133,12 @@ document.addEventListener("DOMContentLoaded", ()=> {
     
         inputPasswordcopy.addEventListener("input", validPassword);
     
-        // La función que se ejecutará cuando el formulario se envíe. Típicamente, esta función realiza validaciones finales y previene el envío del formulario si hay errores.
+        /* La función que se ejecutará cuando el formulario se envíe. Típicamente, esta función realiza validaciones finales y previene el envío del formulario si hay errores.
         
         formRegister.addEventListener("submit", e => {
             e.preventDefault();
             enviarFormulario();
-        });
+        });*/
         
     
     //Objeto de Estado de validación para ocupar en funciones validarCampo, validar contraseña, calcular edad, etc.
@@ -143,7 +160,9 @@ document.addEventListener("DOMContentLoaded", ()=> {
     //Se ocupa .test() para verificar si el valor del campo cumple las restricciones/reglas
     // classList.add añade la clase error
     function validarCampo(regularExpresion, campo, mensaje) {
+        console.log(`Campo: ${campo.id}`)
         const validarCampo = regularExpresion.test(campo.value);
+        console.log(`Resultado de la validacion: ${validarCampo}`)
         if (validarCampo) {
             //console.log("Campo correcto");
             eliminarAlerta(campo.parentElement);
@@ -235,23 +254,29 @@ document.addEventListener("DOMContentLoaded", ()=> {
         }
     
     }
-    
-    
-    
+        
     // Validamos el envío del formulario
-    function enviarFormulario() {
-        if(estadoValidacion.userNames && estadoValidacion.userLastNames && estadoValidacion.userEmail && estadoValidacion.userPhone && estadoValidacion.userAge && estadoValidacion.userResidencia && estadoValidacion.userCity && estadoValidacion.passwords && estadoValidacion.passwordClone) {
-    
+    function enviarFormulario(formData, storedFormData) {
+        if(estadoValidacion.userNames && estadoValidacion.userLastNames && estadoValidacion.userNickname && estadoValidacion.userEmail && estadoValidacion.userPhone && estadoValidacion.userAge && estadoValidacion.userResidencia && estadoValidacion.userCity && estadoValidacion.passwords && estadoValidacion.passwordClone) {
+            
             formRegister.reset();
-            //alertaExito.classList.add("alertaExito");
+            alertaExito.classList.add("alertaExito");
             alertaError.classList.remove("alertaError");
+            alertaUser.classList.remove("alertaUser");
+            alertaCorreo.classList.remove("alertaCorreo");
             setTimeout(() => {
                 alertaExito.classList.remove("alertaExito");
             }, 3000);
+            //Agregar los nuevos datos al array y guardarlos en localStorage
+            storedFormData.push(formData);
+            localStorage.setItem('formData', JSON.stringify(storedFormData));
+            window.location.href = "Iniciar-Sesion.html";
             
         } else {
             alertaExito.classList.remove("alertaExito");
             alertaError.classList.add("alertaError");
+            alertaUser.classList.remove("alertaUser");
+            alertaCorreo.classList.remove("alertaCorreo");
             setTimeout(() =>{
                 alertaError.classList.remove("alertaError");
             }, 3000);
@@ -260,5 +285,5 @@ document.addEventListener("DOMContentLoaded", ()=> {
     }
     });
 
+
     
-    //console.log(alertaExito);
